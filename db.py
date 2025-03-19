c63f21a55327cdfd1d6f19a4879c80341dd42ad3
Rodrigo Lopes Velloso <velloso.rl9@outlook.com>
2025-03-19 16:26:57 -0300

    database

--- db.py
+++ db.py
@@ -1,1 +1,37 @@
 from imports import*
+import sqlite3
+
+# Criando conexão 
+try:
+    con = sqlite3.connect('database.db')
+    print("Conexão com Banco de Dados efetuado com sucesso!")
+except sqlite3.Error as e:
+    print("Erro ao conectar com Banco de Dados!")
+    
+# Criando tabela do banco de dados
+# Tabela Agenda
+try:
+    with con:
+        cur= con.cursor()
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS agenda(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT,
+                ddd TEXT,
+                contato TEXT,
+                email TEXT,
+                nascimento TEXT,
+                idada TEXT,
+                cep TEXT,
+                endereco TEXT,
+                numero text,
+                complemento text,
+                bairro TEXT, 
+                municipio TEXT,
+                estado TEXT,
+                imagem TEXT
+                )""")
+        print("Tabela AGENDA criado com sucesso!")
+except sqlite3.Error as e:
+    print("Erro ao criar Agenda!")
+    
